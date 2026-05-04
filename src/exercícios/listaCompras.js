7331cfab1a785d3d6aba6afa68f5bc459cc40e16
victoria-marques <victoria.marques6@aluno.senai.br>
2026-05-04 15:54:55 -0300

    lista de compras (exercício)

---
+++ src/exercícios/listaCompras.js
@@ -0,0 +1,9 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+export default function ListaCompras(){
+
+    const compras = {
+      id: Date.now().toString(),
+      
+    }
+}
